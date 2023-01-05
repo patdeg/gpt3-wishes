224e3a30608f09bb1a5a00d7eec0a6c710558679
Patrick Deglon <pdeglon@gmail.com>
2023-01-05 13:54:54 -0800

    Update code.js

--- code.js
+++ code.js
@@ -3,49 +3,49 @@
 // Get your OpenAI API Key from https://beta.openai.com/account/api-keys
 // Then click on "create new secret key"
 // Copy & Paste the key (starting with "sk-") below
-var API_KEY = "[your API Key from OpenAI]";
+const API_KEY = "[your API Key from OpenAI]";
 
 // Maximum number of tokens (words) to return (and pay for)
-var NUM_TOKENS = 1024;
+const NUM_TOKENS = 1024;
 
 // Temperature (volalitity, creativity) for the request. 
 // A temperature of 0.0 will always return the same content. 
 // A temperature of 1.0 will maximize the creativity and rerturn very different results at each call. 
 // A temperature of 0.7 is often a good compromise.
-var TEMPERATURE = 0.7;
+const TEMPERATURE = 0.7;
 
-// Function to do the API call
-function _callAPI(prompt) {
-  var data = {
+// Google Sheet function to return a GPT3 suggestion from a prompt 'x' in language 'lang'
+function prompt(x,lang) {
+  
+  // GPT-3 Comletion API Call data/payload:
+  let data = {
     'prompt': prompt,
     'max_tokens': NUM_TOKENS,
     'temperature': TEMPERATURE,
     'model': 'text-davinci-003',
   };
-
-  var options = {
-    'method' : 'post',
-    'contentType': 'application/json',
-    'payload' : JSON.stringify(data),
-    'headers': {
-      Authorization: 'Bearer ' + API_KEY,
-    },
-  };
-
-  response = UrlFetchApp.fetch(
+  
+  // Call GPT-3 API
+  let response = UrlFetchApp.fetchWithExponentialBackoff(
     'https://api.openai.com/v1/completions',
-    options,
+    {
+      'method' : 'post',
+      'contentType': 'application/json',
+      'payload' : JSON.stringify(data),
+      'headers': {
+        Authorization: 'Bearer ' + API_KEY,
+      },
+    },
   );
-
-  return JSON.parse(response.getContentText())['choices'][0]['text']
-}
-
-// Google Sheet function to return a GPT3 suggestion from a prompt 'x' in language 'lang'
-function prompt(x,lang) {
-  var response = _callAPI(x);
+  
+  // Exctract answer in plain text from the response
+  let answer = JSON.parse(response.getContentText())['choices'][0]['text']
+  
+  // Return the answer (in English) is language is 'EN' (English)
   if (lang == "EN") {
-      return response;
+      return answer;
   }
-  return LanguageApp.translate(response, 'en', lang);
+  // Else return the Google translation to 'lang' language
+  return LanguageApp.translate(answer, 'en', lang);
 }
 
